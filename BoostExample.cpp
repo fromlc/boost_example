8eb3d3c84c330be1ce8d996666ac223cad61c603
L Carver <wifihum@gmail.com>
2023-05-11 13:36:49 -0700

    output newline after result, add app banner

--- BoostExample.cpp
+++ BoostExample.cpp
@@ -1,4 +1,8 @@
 #include <boost/lambda/lambda.hpp>
+//#include <boost/lexical_cast.hpp>
+//using boost::lexical_cast;
+//using boost::bad_lexical_cast;
+
 #include <iostream>
 #include <iterator>
 #include <algorithm>
@@ -6,8 +10,11 @@
 int main()
 {
     using namespace boost::lambda;
+
+    std::cout << "boost demo: istream_iterator multiplies by 3\n\n";
+
     typedef std::istream_iterator<int> in;
 
     std::for_each(
-        in(std::cin), in(), std::cout << (_1 * 3) << " ");
+        in(std::cin), in(), std::cout << (_1 * 3) << "\n");
 }
