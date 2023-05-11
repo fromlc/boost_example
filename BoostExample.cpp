b50d908b71b94d3f56a4ee0657494ae08b6e1894
L Carver <wifihum@gmail.com>
2023-05-11 14:01:40 -0700

    update app banner, adjust output

--- BoostExample.cpp
+++ BoostExample.cpp
@@ -1,7 +1,4 @@
 #include <boost/lambda/lambda.hpp>
-//#include <boost/lexical_cast.hpp>
-//using boost::lexical_cast;
-//using boost::bad_lexical_cast;
 
 #include <iostream>
 #include <iterator>
@@ -11,10 +8,12 @@ int main()
 {
     using namespace boost::lambda;
 
-    std::cout << "boost demo: istream_iterator multiplies by 3\n\n";
+    std::cout << "boost demo: istream_iterator multiplies by 3\n";
+    std::cout << "enter numbers separated by a space\n";
+    std::cout << "type the enter key after the last number on a line\n\n";
 
     typedef std::istream_iterator<int> in;
 
     std::for_each(
-        in(std::cin), in(), std::cout << (_1 * 3) << "\n");
+        in(std::cin), in(), std::cout << (_1 * 3) << "\n\n");
 }
